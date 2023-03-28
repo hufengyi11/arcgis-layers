8e84cb6222a8ebd835fdf001d684ab996518db93
hufengyi11 <hufengyi_work@icloud.com>
2023-03-28 13:16:28 +0100

    new url

---
+++ src/components/hooks.js
@@ -0,0 +1,45 @@
+import { useEffect } from "react";
+import {loadModules} from "esri-loader";
+
+export const useCreateMap = (mapRef) => {
+  useEffect(() => {
+    let view;
+
+    const initializeMap = async (mapRef) => {
+      const modules = [
+        "esri/Map",
+        "esri/views/MapView",
+        "esri/layers/GeoJSONLayer"
+      ];
+      const [Map, MapView, GeoJSONLayer] = await loadModules(modules);
+      
+      const map = new Map({ 
+        basemap: "topo-vector" 
+      });
+      
+      view = new MapView({ 
+        container: mapRef.current,
+        map: map,
+        zoom: 11,
+        center: [-0.1278, 51.5074] // London coordinates
+      });
+      
+      // const url =
+      //   "ui/src/components/132kv-overhead-lines.geojson";
+
+      const url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
+
+
+      const geojsonLayer = new GeoJSONLayer({
+        url: url,
+        title: "Power Network Lines"
+      });
+      
+      map.add(geojsonLayer);
+    };
+
+    initializeMap(mapRef);
+
+    return () => view?.destroy();
+  }, [mapRef]);
+};

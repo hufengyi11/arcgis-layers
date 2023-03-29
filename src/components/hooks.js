9d9ea06d9295e26c5d0f13a0a56120785e3b176c
hufengyi11 <hufengyi_work@icloud.com>
2023-03-29 12:14:36 +0100

    added 132kV line

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -8,10 +8,10 @@ export const useCreateMap = (mapRef) => {
     const initializeMap = async (mapRef) => {
       const modules = [
         "esri/Map",
-        "esri/views/MapView", 
-        "esri/Graphic"
+        "esri/views/MapView",
+        "esri/layers/FeatureLayer"
       ];
-      const [Map, MapView, Graphic] = await loadModules(modules);
+      const [Map, MapView, FeatureLayer] = await loadModules(modules);
       
       const map = new Map({ 
         basemap: "topo-vector" 
@@ -20,39 +20,17 @@ export const useCreateMap = (mapRef) => {
       view = new MapView({ 
         container: mapRef.current,
         map: map,
-        zoom: 11,
+        zoom: 8,
         center: [-0.1278, 51.5074] // London coordinates
       });
 
-      const polyline = {
-        type: "polyline", // autocasts as new Polyline()
-        paths: [[-111.3, 52.68], [-98, 49.5], [-93.94, 29.89]]
-      };
-
-      const polylineGraphic = new Graphic({
-        geometry: polyline
+      const fl = new FeatureLayer({
+        portalItem: {  
+          id: "54d29ca67d1c4d269677274b88734b8f" // 132kV
+        }
       });
-
-      view.graphics.add(polylineGraphic)
-    
       
-      // const url =
-      //   "http://localhost:8080/";
-
-      // const geojsonLayer = new GeoJSONLayer({
-      //   url: url,
-      //   title: "Power Network Lines",
-      //   renderer: {
-      //     type: "simple",
-      //     symbol: {
-      //       type: "simple-line",
-      //       color: [255, 0, 0, 1],
-      //       width: 2
-      //     }
-      //   }
-      // });
-      
-      // map.add(geojsonLayer);
+      map.add(fl);
     };
 
     initializeMap(mapRef);

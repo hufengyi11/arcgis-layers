870fbc7d9055a8959f1700696b5f24297578132a
hufengyi11 <hufengyi_work@icloud.com>
2023-03-30 12:08:30 +0100

    added line renderer and scene viewers

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -9,12 +9,13 @@ export const useCreateMap = (mapRef) => {
       const modules = [
         "esri/Map",
         "esri/views/MapView",
-        "esri/layers/FeatureLayer"
+        "esri/layers/FeatureLayer",
       ];
+      
       const [Map, MapView, FeatureLayer] = await loadModules(modules);
       
       const map = new Map({ 
-        basemap: "topo-vector" 
+        basemap: "topo-vector"
       });
       
       view = new MapView({ 
@@ -24,13 +25,24 @@ export const useCreateMap = (mapRef) => {
         center: [-0.1278, 51.5074] // London coordinates
       });
 
+      const lineRenderer = {
+        type: "simple",
+        symbol: {
+          color: "#FFA500",
+          type: "simple-line",
+          style: "solid"
+        }
+      }
+
       const fl = new FeatureLayer({
         portalItem: {  
           id: "54d29ca67d1c4d269677274b88734b8f" // 132kV
-        }
+        },
+        renderer: lineRenderer
       });
-      
-      map.add(fl);
+
+      map.add(fl)
+
     };
 
     initializeMap(mapRef);

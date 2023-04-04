9e76f8618f63626eca3062fa58fa25af6791eaea
hufengyi11 <hufengyi_work@icloud.com>
2023-04-04 10:04:40 +0100

    used load module

--- src/components/map.js
+++ src/components/map.js
@@ -1,10 +1,35 @@
-import React, { useRef } from 'react';
-import { useCreateMap } from './hooks';
+import { useEffect } from "react";
+import { loadModules } from "esri-loader";
 
 const Map = () => {
-  const mapRef = useRef(null);
-  useCreateMap(mapRef);
-  return <div id="viewDiv" ref={mapRef} />;
+  useEffect(() => {
+    loadModules([
+      "esri/WebScene",
+      "esri/views/SceneView",
+      "esri/widgets/Legend"
+    ]).then(([WebScene, SceneView, Legend]) => {
+      const webscene = new WebScene({
+        portalItem: {
+          id: "c331bd42f3544e9fa39ed4289f5c254b"
+        }
+      });
+
+      const view = new SceneView({
+        container: "viewDiv",
+        map: webscene
+      });
+
+      const legend = new Legend ({
+        view:view
+      });
+
+      view.ui.add(legend, "top-right");
+    }).catch(err => {
+      console.error(err);
+    })
+  }, []);
+
+  return <div id="viewDiv" />;
 };
 
 export default Map;

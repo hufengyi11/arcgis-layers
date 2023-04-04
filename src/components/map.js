1ccda4e7417333db2ee198f19db0a6a5ae8c4248
hufengyi11 <hufengyi_work@icloud.com>
2023-04-04 10:39:14 +0100

    added search

--- src/components/map.js
+++ src/components/map.js
@@ -10,8 +10,9 @@ function Map() {
       "esri/WebScene",
       "esri/views/SceneView",
       "esri/widgets/Legend",
+      'esri/widgets/Search',
       'esri/widgets/LayerList'
-    ]).then(([WebScene, SceneView, Legend, LayerList]) => {
+    ]).then(([WebScene, SceneView, Search, Legend, LayerList]) => {
       const webscene = new WebScene({
         portalItem: {
           id: "c331bd42f3544e9fa39ed4289f5c254b"
@@ -23,12 +24,15 @@ function Map() {
         map: webscene
       });
 
-      const legend = new Legend({
+      const search = new Search({
         view: view
       });
 
-      view.ui.add(legend, "top-right");
-
+      view.ui.add(search, {
+        position: 'top-left',
+        index: 0
+      });
+      
       // Add layer list widget
       const layerList = new LayerList({
         view: view
@@ -37,6 +41,12 @@ function Map() {
       view.ui.add(layerList, {
         position: 'top-right'
       });
+
+      const legend = new Legend({
+        view: view
+      });
+
+      view.ui.add(legend, "top-right");
 
       return () => {
         if (view) {
